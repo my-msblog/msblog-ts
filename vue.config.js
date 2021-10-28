33074f5e73390c716ff30bd8b858356af95432b8
maoshen5478 <361193645@qq.com>
2021-10-28 22:34:38 +0800

    chore: add svg-sprite-loader

--- vue.config.js
+++ vue.config.js
@@ -7,6 +7,15 @@ module.exports = {
   outputDir: 'dist',
   assetsDir: 'assets',
   chainWebpack: config => {
+    const svgRule = config.module.rule('svg');
+    svgRule.uses.clear();
+    svgRule
+      .use('svg-sprite-loader')
+      .loader('svg-sprite-loader')
+      .options({
+        symbolId: 'icon-[name]'
+      })
+      .end();
     config.resolve.symlinks(true);
     config
       .plugin('html')
